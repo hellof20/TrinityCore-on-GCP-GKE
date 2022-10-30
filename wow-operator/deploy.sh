7881b3c5fef2f2a6c75308db18f20282f50214e6
Pan, Wen-Ming <smalljelly2018@gmail.com>
2022-10-31 01:42:05 +0800

    Delete deploy.sh

--- wow-operator/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker build -t asia.gcr.io/gcp-wow/wow-operator:v1 .
-docker push asia.gcr.io/gcp-wow/wow-operator:v1
-kubectl delete -f wow-operator-pod.yaml
-kubectl apply -f wow-operator-pod.yaml
