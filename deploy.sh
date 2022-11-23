dcc1289b9b0062d02858aea95353790c8621639d
pwm <pwm@google.com>
2022-11-23 23:17:23 +0800

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "begin to deploy GKE using gcloud ..."
+gcloud container clusters create $name \
+    --cluster-version=1.23 \
+    --no-enable-autoupgrade \
+    --machine-type=e2-standard-4 \
+    --num-nodes=1 \
+    --zone $zone \
+    --project=$project_id
+
+
+kubectl apply -f wow-operator/crd/
+kubectl apply -f wow-operator/wow-operator.yaml
+sleep 60
+kubectl apply -f wow/wow.yaml
