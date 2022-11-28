7a6416095754ef0ddddd976d1d2c7f801c749ddb
pwm <pwm@google.com>
2022-11-28 18:43:15 +0800

    modify destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -2,6 +2,11 @@
 
 echo "begin to destroy..."
 kubectl delete -f wow/wow.yaml
+
+echo "deleting gke cluster"
 gcloud container clusters delete $name \
     --zone $zone \
-    --project=$project_id
+    --project=$project_id \
+    --quiet
+
+echo "complete"
