5710ecc6abc308c55b3ce956facfd11d0d6283e0
Pan, Wen-Ming <smalljelly2018@gmail.com>
2022-12-14 16:21:21 +0800

    Update destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 echo "begin to destroy..."
+
+echo "get gke credential"
+gcloud container clusters get-credentials $name \
+    --zone $zone \
+    --project=$project_id \
+    --quiet
+
+echo "deleting Wow ..."
 kubectl delete -f wow/wow.yaml
 
-echo "deleting gke cluster"
+echo "deleting GKE cluster"
 gcloud container clusters delete $name \
     --zone $zone \
     --project=$project_id \
